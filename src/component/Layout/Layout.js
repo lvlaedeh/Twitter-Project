2c39973a9c56b0a589848693b4a1d22515c456b6
maedeh <maedehDoabii@gmail.com>
2021-06-16 14:22:38 +0430

    start project

---
+++ src/component/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Layout = () => {
+    return (
+        <div>
+            <div>rightSidbar</div>
+            <div>main</div>
+            <div>leftSidbar</div>
+        </div>
+    )
+}
+
+export default Layout
